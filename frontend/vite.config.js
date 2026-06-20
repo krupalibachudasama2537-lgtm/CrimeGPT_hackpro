d9ae39a44e1227339f4990c54c6b0e29c7c6e330
KRUPALIBA CHUDASAMA <krupalibachudasama2537@gmail.com>
2026-06-20 13:05:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
     }
-  }
-})
+)
